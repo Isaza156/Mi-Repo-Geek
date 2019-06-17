e8fb1fa55631f36c33ab61f810f726619ad910a2
Sala 310 <sala310@poliacademico.edu.loc>
2019-06-17 12:34:36 -0500

    act

---
+++ retos_javascript/35-reto.js
@@ -0,0 +1,9 @@
+// juego de palabras
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+        var result = "Mi " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
+  
+        return result;
+  }
+  
+    wordBlanks("dog", "big", "ran", "quickly");

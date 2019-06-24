0274d72d84ddedc554e5ad9c342700fc34274240
Isaza156 <jonathan.isaza@hotmail.com>
2019-06-24 18:03:32 -0500

    act

--- Apuntes/importante.js
+++ Apuntes/importante.js
@@ -130,3 +130,26 @@ arr.unshift([-3]);
 3 !== '3' // verdadero 
 4 !== 3 // verdadero
 
+
+// palabra let para definir variables, limite sus funciones por bloques 
+
+function varTest() {
+    var x = 31;
+    if (true) {
+      var x = 71;  // ¡misma variable!
+      console.log(x);  // 71
+    }
+    console.log(x);  // 71
+  }
+  
+  function letTest() {
+    let x = 31;
+    if (true) {
+      let x = 71;  // variable diferente
+      console.log(x);  // 71
+    }
+    console.log(x);  // 31
+  }
+  // llamamos a las funciones
+  varTest();
+  letTest();
--- retos_javascript/retos.js
+++ retos_javascript/retos.js
@@ -287,3 +287,426 @@ function testStrictNotEqual(val) {
 
 // Change this value to test
 testStrictNotEqual(10);
+
+
+// reto 62, operador mayor >
+
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+  
+  if (val > 10 ) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+// Change this value to test
+testGreaterThan(10);
+
+
+// reto 63, usando operador mayor o igual >=
+
+function testGreaterOrEqual(val) {
+  if (val >= 20 ) {  // Change this line
+    return "20 or Over";
+  }
+  
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
+
+
+// reto 64, usando operador menor <
+
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+  
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+// Change this value to test
+testLessThan(10);
+
+
+// reto 65,usando operador menor o igual <=
+
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+  
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+// Change this value to test
+testLessOrEqual(10);
+
+
+// reto 66, operador &&
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val >= 25 && val <= 50) {
+  
+      return "Yes"; 
+  
+    // Only change code above this line
+}
+return "No";
+}
+  
+// Change this value to test
+testLogicalAnd(10);
+
+
+// reto 67, operador ||
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val > 20 || val < 10) {
+    return "Outside";
+  }
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
+
+
+// reto 68, funcion else
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+  
+  if (val > 5) {
+    result = "Bigger than 5";
+  }else {
+  result = "5 or Smaller";
+  }
+    
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
+
+
+// reto 69, usando else if
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+   return "Smaller than 5";
+        }  else{
+
+return "Between 5 and 10";
+        }
+
+    }  
+
+// Change this value to test
+testElseIf(7);
+
+
+// reto 70, orden de los else if
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+// Change this value to test
+orderMyLogic(7);
+
+
+// reto 71, multiples else if
+
+function testSize(num) {
+  // Only change code below this line
+  if ( num < 5){
+
+    return "Tiny"
+  } else if (num < 10){
+
+return "Small"
+
+  }else if ( num < 15){
+
+return "Medium"
+
+  }else if(num < 20 ){
+
+return "Large"
+
+  }else
+    
+  return "Huge";
+  // Only change code above this line
+}
+
+// Change this value to test
+testSize(20);
+
+
+// reto 72, codigo de golf
+
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1){
+    return "Hole-in-one!";
+  } else if (strokes <= par -2){
+    return "Eagle";
+  } else if (strokes == par -1) {
+    return "Birdie";
+  } else if (strokes == par) {
+    return "Par";
+  } else if (strokes == par +1) {
+    return "Bogey";
+  } else if (strokes == par +2) {
+    return "Double Bogey";
+  } else {
+    return "Go Home!";
+  }
+  // Only change code above this line
+}
+// Change these values to test
+golfScore(5, 4);
+
+
+// reto 73, usando case
+
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+ switch(val){
+
+case 1:
+
+return "alpha"
+break
+
+case 2:
+
+return "beta"
+break
+
+case 3:
+
+return "gamma"
+break
+
+case 4:
+
+return "delta"
+break
+
+ }
+ 
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+caseInSwitch(1);
+
+
+
+// reto 74, usando case con default
+
+function switchOfStuff(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val){
+
+case "a":
+
+return "apple";
+
+break;
+
+case "b":
+
+return "bird";
+
+break;
+
+case "c":
+
+return "cat";
+
+break;
+
+default:
+
+return "stuff"
+
+break;
+  }
+  
+  
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+switchOfStuff(1);
+
+
+
+// reto 75, casos con respuestas iguales
+
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val) {
+  case 1:
+  case 2:
+  case 3:
+    return "Low";
+    break;
+  case 4:
+  case 5:
+  case 6:
+    return "Mid";
+    break;
+  case 7:
+  case 8:
+  case 9:
+return "High";
+break;
+}
+ 
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+sequentialSizes(1);
+
+
+// reto 76, Reemplazo de cadenas de otro tipo con interruptor
+
+function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+switch (val){
+
+case "bob":
+return "Marley"
+break
+
+case 42:
+return "The Answer"
+break
+
+case 1:
+return "There is no #1"
+break
+
+case 99:
+return "Missed me by this much!"
+break
+
+case 7:
+return "Ate Nine"
+break
+
+case "John":
+return ""
+break
+
+case "156":
+return ""
+break
+
+}
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+chainToSwitch(7);
+
+
+
+// reto 77,
+
+
+
+
+// reto 78,
+
+
+
+
+
+// reto 79,
+
+
+
+
+// reto 80,
+
+
+
+
+
+// reto 62,
+
+
+
+
+// reto 62,
+
+
+
+
+
+// reto 62,
+
+
+
+
+// reto 62,
+
+
+
+
+
+// reto 62,
+
+
+
+
+// reto 62,
